ce53799dbce2ca5bedddf02e962c78d8f6fd3e0c
Lukas Willems <lukas.willems@fom-net.de>
2022-03-17 20:46:07 +0100

    add: Überprüfung Geheimzahl zu groß

--- Geheimzahl.py
+++ Geheimzahl.py
@@ -8,5 +8,7 @@
 	if eingabe < geheimzahl:
             print("Ihre Eingabe war zu klein")
         zaehler = zaehler +1
+        if eingabe > geheimzahl:
+            print("Ihre Eingabe war zu groß")
 
     print("Sie haben die Zahl erraten")
