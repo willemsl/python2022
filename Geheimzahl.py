b2f0c7bb3abff10ecb8e87ff220cd65542a547fb
Lukas Willems <lukas.willems@fom-net.de>
2022-03-17 20:44:28 +0100

    add: Programmbasis

---
+++ Geheimzahl.py
@@ -0,0 +1,10 @@
+    geheimzahl = 1337
+    eingabe = 0
+    zaehler = 0
+
+    while eingabe != geheimzahl:
+        eingabe = int(input("Bitte eine Ganzzahl eingeben: "))
+
+        zaehler = zaehler +1
+
+    print("Sie haben die Zahl erraten")
